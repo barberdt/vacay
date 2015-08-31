996cad123c494efa87e0326add581aa3368d0ed2
Davis Barber <barberdt@gmail.com>
2015-08-30 23:07:01 -0400

    Move TripActions/Store/API to shared directory

---
+++ app/screens/App/shared/actions/TripActions.js
@@ -0,0 +1,58 @@
+const alt = require('dispatchers/alt');
+const TripAPI = require('utils/TripAPI');
+
+
+/**
+ * Actions related to trips.
+ */
+class TripActions {
+  constructor() {
+    this.generateActions('createSuccess', 'createFailure', 'loadSuccess',
+        'loadFailure');
+  }
+
+  /**
+   * Create a new trip.
+   *
+   * @param {Immutable.Map} trip - The trip to create.
+   * @return {Promise} A promise that will resolve after save success.
+   */
+  create(trip) {
+    this.dispatch();
+
+    return TripAPI.create(trip)
+      .then((resp) => {
+        const { data } = resp;
+        this.actions.createSuccess(data);
+        return Promise.resolve(data);
+      })
+      .catch((resp) => {
+        const { message } = resp;
+        this.actions.createFailure(message);
+        return Promise.reject(message);
+      });
+  }
+
+  /**
+   * Load the trips. Void dispatch.
+   *
+   * @return {Promise} A promise that will resolve after load success.
+   */
+  load() {
+    this.dispatch();
+
+    return TripAPI.load()
+      .then((resp) => {
+        const { trips } = resp.data;
+        this.actions.loadSuccess(trips);
+        return Promise.resolve(trips);
+      })
+      .catch((resp) => {
+        const { message } = resp;
+        this.actions.loadFailure(message);
+        return Promise.reject(message);
+      });
+  }
+}
+
+module.exports = alt.createActions(TripActions);
